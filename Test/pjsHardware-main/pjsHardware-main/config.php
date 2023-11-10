348d3a67d70d0b1c4f88998d7c569bcf7efa019b
Unique_Malik <maliktate@live.com>
2023-11-10 11:14:14 -0500

    Php push

--- Test/pjsHardware-main/pjsHardware-main/config.php
+++ Test/pjsHardware-main/pjsHardware-main/config.php
@@ -2,22 +2,21 @@
 
 
 
-// Phillip 
+// Malik 
 
 
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'ns1.byethost7.org');
-define('DB_USERNAME', 'jsphardw_admin');
-define('DB_PASSWORD', 'pz-;Ry,ePd%W');
-define('DB_NAME', 'jsphardw_idkwhattoputhere');
+$dbhost = "localhost";
+$dbbuser = "root";
+$dbpass "";
+$dbname "JAMS";
  
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if(!$con = mysqli_connect($dbhost,$dbbuser,$dbpass,$dbname))
  
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+{
+    die("ERROR: Could not connect!");
 }
 ?>
