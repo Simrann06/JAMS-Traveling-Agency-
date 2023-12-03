f1cfc94331944174ffd11d9bc8087a8057013d44
siimr <simranjp9@gmail.com>
2023-12-03 02:00:43 -0500

    simranChanges

--- Test/pjsHardware-main/pjsHardware-main/HomePage2.php
+++ Test/pjsHardware-main/pjsHardware-main/HomePage2.php
@@ -245,6 +245,7 @@
         <a href="HomePage2.php">Home</a>
         <a href="packages.html">Packages</a>
         <a href="cart.html">Shopping Cart</a>
+        <a href="http://127.0.0.1:7860">ChatBot Helper</a>
     </nav>
 
     <section class="Homepage"> 
